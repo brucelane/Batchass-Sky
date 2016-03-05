057c9cbb1c0984c541c3d4e313a0c948f8eac019
Bruce LANE <bruce@batchass.fr>
2016-03-05 03:27:34 +0100

    colors

--- assets/shader.frag
+++ assets/shader.frag
@@ -11,9 +11,10 @@ in vec3     gFacetNormal;
 in vec3     gTriDistance;
 in vec3     gPatchDistance;
 in float    gPrimitive;
+in float    iChromatic;
 
 const vec3 lightPosition    = vec3( 0.5, 0.2, 1.0 );
-const vec3 diffuseColor     = vec3( 1.0 );
+const vec3 diffuseColor     = vec3( 0.5, 0.2, 1.0 );
 const vec3 ambientColor     = vec3( 0.025 );
 
 float amplify(float d, float scale, float offset)
@@ -34,6 +35,6 @@ void main()
     float d1    = min(min(gTriDistance.x, gTriDistance.y), gTriDistance.z);
     float d2    = min(min(gPatchDistance.x, gPatchDistance.y), gPatchDistance.z);
     color       = amplify(d1, 40, -0.5) * amplify(d2, 60, -0.5) * color;
-    
+    color.r		= color.r + iChromatic;
     oColor      = vec4( color, 1.0 );
 }
--- src/BatchassSkyApp.cpp
+++ src/BatchassSkyApp.cpp
@@ -1,46 +1,4 @@
-/*
- Copyright (c) 2010-2015, Paul Houx - All rights reserved.
- This code is intended for use with the Cinder C++ library: http://libcinder.org
-
- This file is part of Cinder-Warping.
-
- Cinder-Warping is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- Cinder-Warping is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Cinder-Warping.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-#include "cinder/app/App.h"
-#include "cinder/app/RendererGl.h"
-#include "cinder/gl/gl.h"
-#include "cinder/gl/Texture.h"
-#include "cinder/ImageIo.h"
-#include "cinder/Rand.h"
-
-#include "Warp.h"
-// Settings
-#include "VDSettings.h"
-// Utils
-#include "VDUtils.h"
-// Audio
-#include "VDAudio.h"
-
-using namespace ci;
-using namespace ci::app;
-using namespace ph::warping;
-using namespace std;
-using namespace VideoDromm;
-
-#define IM_ARRAYSIZE(_ARR)			((int)(sizeof(_ARR)/sizeof(*_ARR)))
-
+#include "BatchassSkyApp.h"
 /*
 Tessellation Shader from Philip Rideout
 
@@ -51,50 +9,7 @@ Cinder experimented from Simon Geilfus
 https://github.com/simongeilfus/Cinder-Experiments
 */
 
-class BatchassSkyApp : public App {
-public:
-	static void prepare(Settings *settings);
 
-	void setup() override;
-	void cleanup() override;
-	void update() override;
-	void draw() override;
-
-	void resize() override;
-
-	void mouseMove(MouseEvent event) override;
-	void mouseDown(MouseEvent event) override;
-	void mouseDrag(MouseEvent event) override;
-	void mouseUp(MouseEvent event) override;
-
-	void keyDown(KeyEvent event) override;
-	void keyUp(KeyEvent event) override;
-
-	void updateWindowTitle();
-private:
-	// Settings
-	VDSettingsRef	mVDSettings;
-	// Utils
-	VDUtilsRef		mVDUtils;
-	// Audio
-	VDAudioRef		mVDAudio;
-
-	bool			mUseBeginEnd;
-
-	fs::path		mSettings;
-
-	gl::TextureRef	mImage;
-	WarpList		mWarps;
-
-	Area			mSrcArea;
-
-	gl::BatchRef	mBatch;
-	float			mInnerLevel, mOuterLevel;
-	// fbo
-	void			renderSceneToFbo();
-	gl::FboRef		mRenderFbo;
-
-};
 
 void BatchassSkyApp::prepare(Settings *settings)
 {
@@ -110,12 +25,15 @@ void BatchassSkyApp::setup()
 	// Utils
 	mVDUtils = VDUtils::create(mVDSettings);
 	mVDUtils->getWindowsResolution();
+	setWindowSize(mVDSettings->mRenderWidth, mVDSettings->mRenderHeight);
+	setWindowPos(ivec2(mVDSettings->mRenderX, mVDSettings->mRenderY));
 	// Audio
 	mVDAudio = VDAudio::create(mVDSettings);
 	fs::path waveFile = getAssetPath("") / "batchass-sky.wav";
 	mVDAudio->loadWaveFile(waveFile.string());
-	setWindowSize(mVDSettings->mRenderWidth, mVDSettings->mRenderHeight);
-	setWindowPos(ivec2(mVDSettings->mRenderX, mVDSettings->mRenderY));
+	// Animation
+	mVDAnimation = VDAnimation::create(mVDSettings);
+
 	// create a batch with our tesselation shader
 	auto format = gl::GlslProg::Format()
 		.vertex(loadAsset("shader.vert"))
@@ -166,7 +84,7 @@ void BatchassSkyApp::setup()
 	gl::Fbo::Format fboFormat;
 	//format.setSamples( 4 ); // uncomment this to enable 4x antialiasing
 	mRenderFbo = gl::Fbo::create(mVDSettings->mRenderWidth, mVDSettings->mRenderHeight, fboFormat.colorTexture());
-
+	iChromatic = 1.0f;
 }
 
 void BatchassSkyApp::cleanup()
@@ -193,26 +111,6 @@ void BatchassSkyApp::renderSceneToFbo()
 	gl::ScopedViewport scpVp(ivec2(0), mRenderFbo->getSize());
 	gl::color(Color::white());
 
-	// setup basic camera
-	auto cam = CameraPersp(getWindowWidth(), getWindowHeight(), 60, 1, 1000).calcFraming(Sphere(vec3(0.0f), 1.25f));
-	gl::setMatrices(cam);
-	gl::rotate(getElapsedSeconds() * 0.1f, vec3(0.123, 0.456, 0.789));
-	gl::viewport(getWindowSize());
-
-	// update uniforms
-	mBatch->getGlslProg()->uniform("uTessLevelInner", mInnerLevel);
-	mBatch->getGlslProg()->uniform("uTessLevelOuter", mOuterLevel);
-
-	// bypass gl::Batch::draw method so we can use GL_PATCHES
-	gl::ScopedVao scopedVao(mBatch->getVao().get());
-	gl::ScopedGlslProg scopedShader(mBatch->getGlslProg());
-
-	gl::context()->setDefaultShaderVars();
-
-	if (mBatch->getVboMesh()->getNumIndices())
-		glDrawElements(GL_PATCHES, mBatch->getVboMesh()->getNumIndices(), mBatch->getVboMesh()->getIndexDataType(), (GLvoid*)(0));
-	else
-		glDrawArrays(GL_PATCHES, 0, mBatch->getVboMesh()->getNumIndices());
 
 }
 void BatchassSkyApp::draw()
@@ -231,6 +129,7 @@ void BatchassSkyApp::draw()
 	// update uniforms
 	mBatch->getGlslProg()->uniform("uTessLevelInner", mInnerLevel + mVDSettings->maxVolume/10);
 	mBatch->getGlslProg()->uniform("uTessLevelOuter", mOuterLevel);
+	mBatch->getGlslProg()->uniform("iChromatic", iChromatic);
 
 	// bypass gl::Batch::draw method so we can use GL_PATCHES
 	gl::ScopedVao scopedVao(mBatch->getVao().get());
@@ -278,6 +177,8 @@ void BatchassSkyApp::mouseMove(MouseEvent event)
 	// pass this mouse event to the warp editor first
 	if (!Warp::handleMouseMove(mWarps, event)) {
 		// let your application perform its mouseMove handling here
+		iChromatic = event.getX()/100.0;
+
 	}
 }
 
@@ -350,9 +251,16 @@ void BatchassSkyApp::keyDown(KeyEvent event)
 				mSrcArea = Area(x1, y1, x2, y2);
 			}
 			break;
+		case KeyEvent::KEY_s:
+			// save animation
+			mVDAnimation->save();
+			break;
+		case KeyEvent::KEY_k:
+			// save keyframe
+			mVDAnimation->saveKeyframe(to_string(getElapsedSeconds()));
+			break;
 		case KeyEvent::KEY_SPACE:
-			// toggle drawing mode
-			mUseBeginEnd = !mUseBeginEnd;
+			
 			updateWindowTitle();
 			break;
 		}
@@ -372,7 +280,8 @@ void BatchassSkyApp::keyUp(KeyEvent event)
 
 void BatchassSkyApp::updateWindowTitle()
 {
-	getWindow()->setTitle(to_string((int)getAverageFps()) + " fps Batchass Sky");
+	//getWindow()->setTitle(to_string((int)getAverageFps()) + " fps Batchass Sky");
+	getWindow()->setTitle(to_string(iChromatic) + " fps Batchass Sky");
 }
 
 CINDER_APP(BatchassSkyApp, RendererGl(RendererGl::Options().msaa(8)), &BatchassSkyApp::prepare)
